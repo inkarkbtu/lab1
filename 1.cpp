d2102c9335c55a1d2bbb9c36f4d879036187b857
inkarkbtu <inkar.bakhytkali@gmail.com>
2025-01-25 23:29:40 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b;
+    double c;
+    cin>>a>>b;
+    a=a*a;
+    b=b*b;
+    c=sqrt(a+b);
+    cout<<c;
+}
